3c194a15c9e05947af491b6a0465051fbceb4863
BryanField15 <6367@holbertonstudents.com>
2023-05-09 21:57:25 -0700

    handover

--- _push.c
+++ _push.c
@@ -4,8 +4,43 @@
 
 void _push(stack_t **stack, unsigned int line_no)
 {
-//	int g_push_arg;
- 	
+//	extern int g_push_arg;
+	int i = 6;
+
 	printf("global var in _push func: %d\n", g_push_arg);
+
+	printf("we are in the push func\n");
+	stack_t *new_node;
+
+/*	if (stack == NULL)
+	{
+		return;
+		}*/
+	printf("line 20 flag\n");
+	new_node = malloc(sizeof(new_node));
+	if (new_node == NULL)
+	{
+		return;
+	}
+	printf("line 25 flag\n");
+	new_node->n = i;
+	printf("line 27 flag\n");
+	new_node->next = *stack;
+	printf("line 29 flag\n");
+	new_node->prev = NULL;
+	printf("line 30 flag\n");
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+	printf("line 36 flag\n");
+	printf("this the is new node [%d]",(*stack)->n);
+	while (*stack != NULL)
+	{
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->next;
+	}
+	printf("line 43 flag\n");
 
 }
--- monty.c
+++ monty.c
@@ -21,9 +21,9 @@ int main(int ac, char **av)
 	unsigned int current_line_number = 0;
 	char *line_tokens[2];
 	int func_check;
-	int g_push_arg;
+//	extern int g_push_arg;
 	int num_to_push;
-	
+
 	stack = NULL;
 	if (ac != 2)
 	{
@@ -49,13 +49,10 @@ int main(int ac, char **av)
 		if (is_num(line_tokens[1]) == 1)
 		{
 			num_to_push = atoi(line_tokens[1]);
-			g_push_arg = num_to_push;
-//			printf("global var int inside loop: %d", g_push_arg);
-		}
-		else
-		{
-			g_push_arg = -1;
-		}
+//			g_push_arg = num_to_push;
+			g_push_arg = 42;
+			printf("global var int inside loop: %d", g_push_arg);
+		} 
 		printf("[%u][0]: %s\t[%u][1]: %s\t global var: %d\n", current_line_number, line_tokens[0], current_line_number, line_tokens[1], g_push_arg);
 		func_check = check_op_func(line_tokens[0], current_line_number);
 
