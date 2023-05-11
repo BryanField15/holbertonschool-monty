108c9e6eef64c4774d2ac9aabed2ac31ef2ed06c
BryanField15 <6367@holbertonstudents.com>
2023-05-11 16:58:50 -0700

    is num edge case

--- is_num.c
+++ is_num.c
@@ -3,7 +3,7 @@
 /**
  *is_num - checks if string is number
  *@str: string to test
- *
+ *@line_no: current line number
  *Return: 1 for number, 0 not number
  */
 int is_num(char *str, unsigned int line_no)
@@ -13,7 +13,12 @@ int is_num(char *str, unsigned int line_no)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (isdigit(str[i]) == 0)
+		if (str[i] == '+' || str[i] == '-')
+		{
+			i = i + 1;
+		}
+
+		else if (isdigit(str[i]) == 0)
 		{
 			fprintf(stderr, "L%u: usage: push integer\n", line_no);
 			exit(EXIT_FAILURE);
