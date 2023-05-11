90d2ee7eedb4585a354796223ec43718159b4b60
BryanField15 <6367@holbertonstudents.com>
2023-05-11 05:52:10 -0700

    betty

--- _pop.c
+++ _pop.c
@@ -2,11 +2,11 @@
 
 void _pop(stack_t **stack, unsigned int line_no)
 {
-        if (stack == NULL)
-        {
-                printf("L%u: can't pop an stack empty\n", line_no);
-                exit (EXIT_FAILURE);
-        }
+	if (stack == NULL)
+	{
+		printf("L%u: can't pop an stack empty\n", line_no);
+		exit(EXIT_FAILURE);
+	}
 	if ((*stack)->next == NULL)
 	{
 		(*stack) = NULL;
